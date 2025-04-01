ad74b5e03a3f970ff34464e1cf7e0125453cec35
César Ibarra <cesarij07@gmail.com>
2025-03-31 19:23:22 -0600

    refactor(staticAnalysis.groovy): update script

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,5 +1,7 @@
-//  Ejercicio de pruebas de calidad de código
+//  Ejercicio 4 checar las ramas main y hotfix
 def call(boolean abortPipeline = false) {
+    def branch = env.BRANCH_NAME ?: 'unknown'
+
     timeout(time: 5, unit: 'MINUTES') {
         withEnv(['sonarenv=fake']) {
             sh 'echo "Ejecución de las pruebas de calidad de código con tiempo de espera de 5 minutos"'
@@ -7,6 +9,12 @@ def call(boolean abortPipeline = false) {
     }
 
     if (abortPipeline) {
-        error("Abortado por configuración de abortPipeline = true")
+        error("Abortando pipeline por abortPipeline = true")
+    } else if (branch == 'main') {
+        error("Abortando el pipeline de calidad porque es la rama main")
+    } else if (branch.contains('hotfix')) {
+        error("Abortando el pipeline de calidad porque es la una rama que contiene hotfix")
+    } else {
+        echo "Corriendo el pipeline de calidad, la rama es ${branch}"
     }
 }
