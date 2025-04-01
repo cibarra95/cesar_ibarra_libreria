04e7054c887e8100717b2740ea0a26db691c740b
César Ibarra <cesarij07@gmail.com>
2025-03-31 20:32:49 -0600

    feature(sonarQube): Add sonarqube

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,19 +1,25 @@
-//  Ejercicio 4 checar las ramas main y hotfix
-def call(boolean abortPipeline = false) {
+//  Ejercicio Opcional
+def call(boolean abortPipeline = false, boolean waitForGate = false) {
     def branch = env.BRANCH_NAME ?: 'unknown'
 
-    timeout(time: 5, unit: 'MINUTES') {
-        withEnv(['sonarenv=fake']) {
-            sh 'echo "Ejecución de las pruebas de calidad de código con tiempo de espera de 5 minutos"'
+    if (abortPipeline) {
+            error("Abortando pipeline por abortPipeline = true")
+        }
+
+    withSonarQubeEnv('SonarQube') {
+        sh 'sonar-scanner -Dsonar.projectKey=sharedlib -Dsonar.sources=. -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_AUTH_TOKEN'
+    }
+
+    if (waitForGate) {
+        timeout(time: 5, unit: 'MINUTES') {
+            waitForQualityGate abortPipeline: abortPipeline
         }
     }
 
-    if (abortPipeline) {
-        error("Abortando pipeline por abortPipeline = true")
-    } else if (branch == 'main') {
+     if (branch == 'main') {
         error("Abortando el pipeline de calidad porque es la rama main")
     } else if (branch.contains('hotfix')) {
-        error("Abortando el pipeline de calidad porque es la una rama que contiene hotfix")
+        error("Abortando el pipeline de calidad porque es la rama que contiene hotfix")
     } else {
         echo "Corriendo el pipeline de calidad, la rama es ${branch}"
     }
